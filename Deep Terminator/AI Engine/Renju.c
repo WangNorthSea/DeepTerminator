9ba38ef048040e09e7fccf9d600e4b34e2393b68
王浩宇 <haoyuwang@wanghaoyudeMacBook-Pro.local>
2018-12-22 20:15:53 +0800

    two rush four forbid bugs fixed some

--- Deep Terminator/AI Engine/Renju.c
+++ Deep Terminator/AI Engine/Renju.c
@@ -13,10 +13,16 @@
 int checkForbidMove(char * board, int index) {
     int i, count = 0;
     unsigned char pats[4];
-    pats[0] = patMap[getPatternCode(board, index, 0)];
-    pats[1] = patMap[getPatternCode(board, index, 1)];
-    pats[2] = patMap[getPatternCode(board, index, 2)];
-    pats[3] = patMap[getPatternCode(board, index, 3)];
+    unsigned int pcode[4];
+    pcode[0] = getPatternCode(board, index, 0);
+    pcode[1] = getPatternCode(board, index, 1);
+    pcode[2] = getPatternCode(board, index, 2);
+    pcode[3] = getPatternCode(board, index, 3);
+    
+    pats[0] = patMap[pcode[0]];
+    pats[1] = patMap[pcode[1]];
+    pats[2] = patMap[pcode[2]];
+    pats[3] = patMap[pcode[3]];
     
     for (i = 0; i < 4; i++) {
         if ((pats[i] & 15) == 5)
@@ -24,6 +30,17 @@ int checkForbidMove(char * board, int index) {
     }
     if (count >= 2)
         return 1;
+    
+    //双冲四特判
+    for (i = 0; i < 4; i++)
+        if (pcode[i] == 70928 || pcode[i] == 595216 || pcode[i] == 70936 || pcode[i] == 595224)
+            return 1;
+    for (i = 0; i < 4; i++)
+        if (pcode[i] == 332880 || pcode[i] == 2430032 || pcode[i] == 332888 || pcode[i] == 2430040)
+            return 1;
+    for (i = 0; i < 4; i++)
+        if (pcode[i] == 83220 || pcode[i] == 607508 || pcode[i] == 83222 || pcode[i] == 607510)
+            return 1;
     
     count = 0;
     for (i = 0; i < 4; i++) {
